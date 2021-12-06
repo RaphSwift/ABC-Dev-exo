43d229d50a9deaaaac36b7e19706bee4c84e36f5
Raph <30597735+RaphSwift@users.noreply.github.com>
2021-12-06 16:43:05 +0100

    ExYT

---
+++ eclipse-workspace/exOnYOUTUBE/src/exOnYOUTUBE/javabasicsYouTube.java
@@ -0,0 +1,13 @@
+package exOnYOUTUBE;
+
+public class javabasicsYouTube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String exclaim = addExclamationPoint("Xmas greetings");
+		System.out.println(exclaim);
+	}
+	public static String addExclamationPoint(String s) {
+		return s + "!";
+	}
+}
